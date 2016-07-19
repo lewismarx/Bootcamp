481c1a1d231c3fa85d23a016ccf83af155725688
lewis marx <lewis marx>
2016-07-19 13:41:08 -0500

    Initial commit

---
+++ test.js
@@ -0,0 +1,8 @@
+//noinspection JSUnresolvedFunction
+/**
+ * Created by lowrc on 7/19/2016.
+ */
+ $('#h1button').on('click', function() {alert('You Have Won');
+});
+$('input').keypress(function() {alert("hello world")});
+$("li").on('click', function(){$(this).remove();});
